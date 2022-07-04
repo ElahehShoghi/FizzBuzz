1219063ee3ef76c9ae4a2d6f9ea545719b839382
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-04 13:17:10 +0430

    Refactor fizzbuzz() to return once.

--- src/main/kotlin/com/tdd/FizzBuzz.kt
+++ src/main/kotlin/com/tdd/FizzBuzz.kt
@@ -3,12 +3,13 @@ package com.tdd
 class FizzBuzz {
 
     fun fizzbuzz(input: Int): String {
+        var result = input.toString()
         if (input % 15 == 0)
-            return "FizzBuzz"
-        if (input % 3 == 0)
-            return "Fizz"
-        if (input % 5 == 0)
-            return "Buzz"
-        return input.toString()
+            result = "FizzBuzz"
+        else if (input % 3 == 0)
+            result = "Fizz"
+        else if (input % 5 == 0)
+            result = "Buzz"
+        return result
     }
 }
