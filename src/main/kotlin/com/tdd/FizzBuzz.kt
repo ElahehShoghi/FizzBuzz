543cc446105e816d1b9c8e0f8dc5adc0664f7734
Elaheh Shoghi <e.shoghi@rahkar.co>
2022-07-04 13:10:58 +0430

    Pass the test case to assert that return "FizzBuzz" for multiplies of three and five numbers.

--- src/main/kotlin/com/tdd/FizzBuzz.kt
+++ src/main/kotlin/com/tdd/FizzBuzz.kt
@@ -3,6 +3,8 @@ package com.tdd
 class FizzBuzz {
 
     fun fizzbuzz(input: Int): String {
+        if (input % 15 == 0)
+            return "FizzBuzz"
         if (input % 3 == 0)
             return "Fizz"
         if (input % 5 == 0)
